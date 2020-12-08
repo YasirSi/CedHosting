610e8944825f4011d54b587ea2b075e87ec2adfe
cedcoss <yasirsiddiqui757@gmail.com>
2020-12-08 15:42:44 +0530

    initial commit

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 								<i class="icon-bar"></i>
 							</button>				  
 							<div class="navbar-brand">
-								<img src="l.png" alt="">
+								<img src="l.png" alt="" style="margin-top:-90px;">
 								<!-- <h1><a href="index.php">Planet Hosting</a></h1> -->
 							</div>
 						</div>
@@ -26,12 +26,12 @@
 								<li class="dropdown">
 									<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services<i class="caret"></i></a>
 										<ul class="dropdown-menu">
-											<li><a href="faq.php">FAQ's</a></li>
+											<!-- <li><a href="faq.php">FAQ's</a></li>
 											<li><a href="testimonials.php">Testimonials</a></li>
 											<li><a href="history.php">History</a></li>
 											<li><a href="support.php">Support</a></li>
 											<li><a href="templatesetting.php">Template setting</a></li>
-											<li><a href="portfolio.php">Portfolio</a></li>
+											<li><a href="portfolio.php">Portfolio</a></li> -->
 										</ul>
 									</li>
 								<li class="dropdown">
@@ -46,7 +46,10 @@
 								<li><a href="pricing.php">Pricing</a></li>
 								<li><a href="blog.php">Blog</a></li>
 								<li><a href="contact.php">Contact</a></li>
-								<li><a href="login.php">Cart</a></li>
+								<li><a href=""><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cart" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+</svg></a></li>
+								<!-- <li><a href="login.php">Cart</a></li> -->
 								<li><a href="login.php">Login</a></li>
 
 							</ul>
